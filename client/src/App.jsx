9efeb57e8d0228bfe0e9b6799fc18bcb6a5350c8
amStevee <stevenanongo2@gmail.com>
2026-05-01 16:24:49 -0700

    configure tailwindcss

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <>
       <h1 className="bg-amber-950">Hello World</h1>
+      <Button>Click</Button>
     </>
   );
 }
---
+++ server/src/server.js
@@ -0,0 +1,1 @@
+console.log("serever is running..");
