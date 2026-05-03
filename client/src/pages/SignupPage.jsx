bd464def82ca1636f6bbd756db3c6b6e93b5d918
amStevee <stevenanongo2@gmail.com>
2026-05-03 02:09:42 -0700

    set up signup page

---
+++ client/src/pages/SignupPage.jsx
@@ -0,0 +1,9 @@
+export default function SignupPage() {
+  return (
+    <div className="w-full h-screen flex flex-col items-center pt-5">
+      <h1 className="text-primary font-bold text-[clamp(1.5rem,5vw,3rem)]">
+        Create Account
+      </h1>
+    </div>
+  );
+}
