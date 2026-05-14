8e9848f68aee041faef7fbe5882feaeed8e68f23
amStevee <stevenanongo2@gmail.com>
2026-05-13 18:20:38 -0700

    bug/fix form-margin

--- client/src/pages/SignupPage.jsx
+++ client/src/pages/SignupPage.jsx
@@ -63,7 +63,7 @@ export default function SignupPage() {
         <small>Be a part of the Buddy hub community</small>
       </div>
 
-      <FieldGroup onSubmit={handleSubmit} className="mx-2.5">
+      <FieldGroup onSubmit={handleSubmit} className="mx-5">
         {/* Firstname */}
         <Field>
           <FieldLabel htmlFor="firstname">
