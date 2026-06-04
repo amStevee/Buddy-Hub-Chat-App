8f1b660e5b807ef875ebceeaa5ca18198a5e2307
amStevee <stevenanongo2@gmail.com>
2026-06-03 17:05:24 -0700

    add docs and prisma

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,8 @@
-console.log("Server is running..");
+import express from "express";
+import v1Routes from "./api/v1/routes.js";
+
+const app = express();
+
+app.use(express.json());
+
+export default app;
--- server/src/server.js
+++ server/src/server.js
@@ -1,1 +1,8 @@
-console.log("serever is running..");
+import http from 'node:http';
+import app from './app.js'
+
+const server = http.createServer(app);
+
+server.listen(8000, () => {
+    console.log('server is running on port')
+})
