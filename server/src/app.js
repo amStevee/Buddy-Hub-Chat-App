193f1de4ce79429d65287838844cf986cc4aa09a
amStevee <stevenanongo2@gmail.com>
2026-07-14 15:46:07 -0700

    security/ add rate-limit

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,11 @@
 import express from "express";
+import helmet from "helmet";
+import cors from "cors";
+import rateLimit from "express-rate-limit";
 import v1Routes from "./api/v1/routes.js";
 import { errorMiddleware } from "./core/ErrorHandler.js";
 import path from "path";
 import { fileURLToPath } from "url";
-import cors from "cors";
-import helmet from "helmet";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -14,12 +15,22 @@ const corsOptions = {
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 100,
+  limit: 100,
+  standardHeaders: "draft-8",
+  legacyHeaders: false,
+  ipv6Subnet: 56,
+});
+
 const app = express();
 
 app.use(helmet());
 app.use(cors(corsOptions));
+app.use(limiter);
 
 app.use(express.json());
+
 
 const clientBuildPath = path.join(__dirname, "../../client/dist");
 app.use(express.static(clientBuildPath));
